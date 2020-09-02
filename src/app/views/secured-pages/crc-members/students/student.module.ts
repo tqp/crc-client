08df484cc4597042dc8eafc0132cd1128f835b95
tqp <timothyqpowers@gmail.com>
2020-09-02 13:23:25 -0500

    Interim commit before phone conversation on 20200902. - Minor error fixed - Custom color scheme changed for demo

--- src/app/views/secured-pages/crc-members/students/student.module.ts
+++ src/app/views/secured-pages/crc-members/students/student.module.ts
@@ -1,17 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { StudentDetailComponent } from './student-detail/student-detail.component';
-import { StudentDetailEditComponent } from './student-detail-edit/student-detail-edit.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { ListAddRemoveItemsBasicModule } from '../../../../../@tqp/components/list-add-remove-items-basic/list-add-remove-items-basic.module';
-import { RouterModule } from '@angular/router';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AngularMaterialModule } from '../../../../../@tqp/modules/angular-material.module';
-import { StudentListComponent } from './student-list/student-list.component';
-import { StudentRoutingModule } from './student-routing.module';
-import { StudentCaregiverRelationbshipEditDialogComponent } from './student-caregiver-relationbship-edit-dialog/student-caregiver-relationbship-edit-dialog.component';
-import { StudentCaregiverEditDialogComponent } from './student-caregiver-edit-dialog/student-caregiver-edit-dialog.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {StudentDetailComponent} from './student-detail/student-detail.component';
+import {StudentDetailEditComponent} from './student-detail-edit/student-detail-edit.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {ListAddRemoveItemsBasicModule} from '../../../../../@tqp/components/list-add-remove-items-basic/list-add-remove-items-basic.module';
+import {RouterModule} from '@angular/router';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AngularMaterialModule} from '../../../../../@tqp/modules/angular-material.module';
+import {StudentListComponent} from './student-list/student-list.component';
+import {StudentRoutingModule} from './student-routing.module';
+import {StudentCaregiverEditDialogComponent} from './student-caregiver-edit-dialog/student-caregiver-edit-dialog.component';
 
 @NgModule({
   declarations: [
