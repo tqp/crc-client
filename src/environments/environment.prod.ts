a3b4c6085cc3c243b69e6b5edcf35ded0507c86a
tqp <timothyqpowers@gmail.com>
2020-09-03 15:43:52 -0500

    Moved server back to load balanced https.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   name: 'prod',
   production: true,
   hmr: false,
-  apiUrl: 'http://crc-server.us-east-1.elasticbeanstalk.com/',
+  apiUrl: 'https://crc-server.timsanalytics.com/',
   buildTimestamp: '08/12/2020 01:00:02'
 };
