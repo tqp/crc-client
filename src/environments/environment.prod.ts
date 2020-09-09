cce5f4a23810e3c774945c90534b34be6fe8739b
Tim <timothyqpowers@gmail.com>
2020-09-09 01:11:07 -0400

    Trying to fix the login issue.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   name: 'prod',
   production: true,
   hmr: false,
-  apiUrl: 'https://crc-server.timsanalytics.com/',
+  apiUrl: 'https://crc-server.timsanalytics.com',
   buildTimestamp: '08/12/2020 01:00:02'
 };
